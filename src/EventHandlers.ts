6ab8d3b1642de2a60aa0406c43e3ec7dd8a89b3d
moose-code <jonjonclark@gmail.com>
2024-03-28 10:01:30 +0000

    Testing indexing and more

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -11,7 +11,7 @@ import {
   UniswapV3Pool_MintEntity,
   UniswapV3Pool_BurnEntity,
   uniswapV3Pool_InitializeEntity,
-  // uniswapV3Pool_SwapEntity,
+  uniswapV3Pool_SwapEntity,
   TickEntity,
 } from "../generated/src/Types.gen";
 
@@ -163,20 +163,19 @@ UniswapV3PoolContract.Mint.handler(({ event, context }) => {
   context.UniswapV3Pool_Mint.set(uniswapV3Pool_MintEntity);
 });
 
-// UniswapV3PoolContract.Swap.loader(({ event, context }) => {});
+UniswapV3PoolContract.Swap.loader(({ event, context }) => {});
 
-// UniswapV3PoolContract.Swap.handler(({ event, context }) => {
-//   const uniswapV3Pool_SwapEntity: uniswapV3Pool_SwapEntity = {
-//     id: event.transactionHash + event.logIndex.toString(),
-//     sender: event.params.sender,
-//     recipient: event.params.recipient,
-//     amount0: event.params.amount0,
-//     amount1: event.params.amount1,
-//     sqrtPriceX96: event.params.sqrtPriceX96,
-//     liquidity: event.params.liquidity,
-//     tick: event.params.tick,
-//     eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
-//   };
+UniswapV3PoolContract.Swap.handler(({ event, context }) => {
+  const uniswapV3Pool_SwapEntity: uniswapV3Pool_SwapEntity = {
+    id: event.transactionHash + event.logIndex.toString(),
+    sender: event.params.sender,
+    recipient: event.params.recipient,
+    amount0: event.params.amount0,
+    amount1: event.params.amount1,
+    sqrtPriceX96: event.params.sqrtPriceX96,
+    liquidity: event.params.liquidity,
+    tick: event.params.tick,
+  };
 
-//   context.UniswapV3Pool_Swap.set(uniswapV3Pool_SwapEntity);
-// });
+  context.UniswapV3Pool_Swap.set(uniswapV3Pool_SwapEntity);
+});
